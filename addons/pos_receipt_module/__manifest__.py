2b384c73f877f324d7ec9f174cdf748fb1327450
ahmadBsat <64708488+ahmadBsat@users.noreply.github.com>
2025-10-08 18:29:19 +0300

    Update __manifest__.py

--- addons/pos_receipt_module/__manifest__.py
+++ addons/pos_receipt_module/__manifest__.py
@@ -1,15 +1,25 @@
 {
     'name': 'POS Custom Receipt',
-    'version': '1.0.3',
+    'version': '18.0.1.0.0',
     'category': 'Point of Sale',
     'summary': 'Customize POS receipt header',
+    'description': """
+        Custom POS Receipt
+        ==================
+        * Custom header with company name
+        * Larger order numbers
+        * Custom styling
+    """,
+    'author': 'Your Name',
     'depends': ['point_of_sale'],
     'assets': {
         'point_of_sale._assets_pos': [
             'pos_receipt_module/static/src/xml/pos_receipt.xml',
+            'pos_receipt_module/static/src/css/pos_receipt.css',
         ],
     },
     'installable': True,
     'application': False,
     'auto_install': False,
+    'license': 'LGPL-3',
 }
